359dc4d36e8cddcd78764e941ba2552082770649
Alperen Şah Aburşum <85456369+alperen-cpu@users.noreply.github.com>
2023-05-16 21:34:17 +0300

    Update s3.php
    yıl, ay, gün formatında yedeğin adına ekleyeceğiz.
    createTarGz adında yeni bir fonksiyon ekledik, belirtilen kaynak dizinini tar.gz formatında sıkıştırır ve hedef dosyaya kaydeder.
    sendBackupToS3 fonksiyonunu güncelledik ve dosyaları sıkıştırılan geçici bir dosya oluşturduk ($tempFile). Ardından, bu geçici dosyayı AWS S3'ye yüklemek için önceki gibi putObject metodunu kullanıyoruz. Son olarak, yedekleme tamamlandıktan sonra geçici dosyayı siliyoruz (unlink($tempFile)).

--- s3.php
+++ s3.php
@@ -10,8 +10,38 @@ define('AWS_REGION', 'your-aws-region');
 define('AWS_BUCKET_NAME', 'your-bucket-name');
 
 
+function createTarGz($source, $destination)
+{
+    $rootPath = realpath($source);
+
+    $zip = new ZipArchive();
+    $zip->open($destination, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($rootPath),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+
+    foreach ($files as $name => $file) {
+        if (!$file->isDir()) {
+            $filePath = $file->getRealPath();
+            $relativePath = substr($filePath, strlen($rootPath) + 1);
+            $zip->addFile($filePath, $relativePath);
+        }
+    }
+
+    $zip->close();
+}
+
 function sendBackupToS3($file)
 {
+    $backupDate = date('Y-m-d');
+    $tempFile = '/tmp/backup_' . $backupDate . '.tar.gz';
+
+    // Dosyaları sıkıştır
+    createTarGz($file, $tempFile);
+
+    // AWS S3'ye yedek gönder
     $s3Client = new S3Client([
         'version' => 'latest',
         'region' => AWS_REGION,
@@ -21,10 +51,12 @@ function sendBackupToS3($file)
         ],
     ]);
 
+    $backupName = 'backup_' . $backupDate . '.tar.gz';
+
     $result = $s3Client->putObject([
         'Bucket' => AWS_BUCKET_NAME,
-        'Key' => basename($file),
-        'SourceFile' => $file,
+        'Key' => $backupName,
+        'SourceFile' => $tempFile,
     ]);
 
     if ($result['ObjectURL']) {
@@ -32,6 +64,9 @@ function sendBackupToS3($file)
     } else {
         echo 'Yedekleme sırasında bir hata oluştu.';
     }
+
+    // Geçici dosyayı sil
+    unlink($tempFile);
 }
 
 // Yedeklemek istenilen dosya veya dizin:
